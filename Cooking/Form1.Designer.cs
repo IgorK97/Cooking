ede137be0fcb75a704ac2a90b6cbe7040e064ad7
IgorK97 <kt.wert.74@gmail.com>
2024-10-26 19:27:58 +0300

    Начало разработки

---
+++ Cooking/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Cooking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.ListBoxNotes = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBoxBookmarks = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(944, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.txtNote);
+            this.tabPage1.Controls.Add(this.ListBoxNotes);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Заметки";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Удалить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(204, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(557, 310);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(204, 7);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(557, 20);
+            this.txtNote.TabIndex = 1;
+            // 
+            // ListBoxNotes
+            // 
+            this.ListBoxNotes.FormattingEnabled = true;
+            this.ListBoxNotes.Location = new System.Drawing.Point(7, 7);
+            this.ListBoxNotes.Name = "ListBoxNotes";
+            this.ListBoxNotes.Size = new System.Drawing.Size(190, 381);
+            this.ListBoxNotes.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.listBoxBookmarks);
+            this.tabPage2.Controls.Add(this.webBrowser1);
+            this.tabPage2.Controls.Add(this.txtSearch);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(936, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Поиск рецептов";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBoxBookmarks
+            // 
+            this.listBoxBookmarks.FormattingEnabled = true;
+            this.listBoxBookmarks.Location = new System.Drawing.Point(7, 33);
+            this.listBoxBookmarks.Name = "listBoxBookmarks";
+            this.listBoxBookmarks.Size = new System.Drawing.Size(284, 316);
+            this.listBoxBookmarks.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(297, 7);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(633, 386);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(7, 7);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(202, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(216, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 355);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 37);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Переименовать закладку";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(121, 356);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 36);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Удалить закладку";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(211, 358);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 36);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Добавить закладку";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Кулинария";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.ListBox ListBoxNotes;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBoxBookmarks;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Cooking/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Cooking
+{
+    public partial class Form1 : Form
+    {
+        List<string> recipeNotes = new List<string>();
+        List<string> bookmarks = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            int BrowserVer, RegVal;
+
+            // get the installed IE version
+            //using (WebBrowser Wb = new WebBrowser())
+                BrowserVer = webBrowser1.Version.Major;
+
+            // set the appropriate IE version
+            if (BrowserVer >= 11)
+                RegVal = 11001;
+            else if (BrowserVer == 10)
+                RegVal = 10001;
+            else if (BrowserVer == 9)
+                RegVal = 9999;
+            else if (BrowserVer == 8)
+                RegVal = 8888;
+            else
+                RegVal = 7000;
+
+            // set the actual key
+            using (RegistryKey Key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", RegistryKeyPermissionCheck.ReadWriteSubTree))
+                if (Key.GetValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe") == null)
+                    Key.SetValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", RegVal, RegistryValueKind.DWord);
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadRecipeNotes();
+            LoadBookmarks();
+        }
+        private void LoadRecipeNotes()
+        {
+            if (File.Exists("notes.txt"))
+            {
+                recipeNotes.AddRange(File.ReadAllLines("recipeNotes.txt"));
+                ListBoxNotes.DataSource=new BindingSource(recipeNotes,null);
+            }
+        }
+
+        private void LoadBookmarks()
+        {
+            if (File.Exists("bookmarks.txt"))
+            {
+                bookmarks.AddRange(File.ReadAllLines("bookmarks.txt"));
+                listBoxBookmarks.DataSource = new BindingSource(bookmarks, null);
+            }
+        }
+
+        private void SaveRecipeNotes()
+        {
+            File.WriteAllLines("notes.txt", recipeNotes);
+        }
+
+        private void SaveBookmarks()
+        {
+            File.WriteAllLines("bookmarks.txt", bookmarks);
+        }
+
+        private void btnAddNote_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtNote.Text))
+            {
+                recipeNotes.Add(txtNote.Text);
+                ListBoxNotes.DataSource = new BindingSource(recipeNotes, null);
+            }
+        }
+
+        private void btnDeleteNote_Click(object sender, EventArgs e)
+        {
+            if (ListBoxNotes.SelectedItem != null)
+            {
+                recipeNotes.Remove(ListBoxNotes.SelectedItem.ToString());
+                ListBoxNotes.DataSource = new BindingSource(recipeNotes, null);
+                SaveRecipeNotes();
+            }
+        }
+
+        private void btnSearchRecipes_Click(object sender, EventArgs e)
+        {
+            string query = txtSearch.Text;
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                webBrowser1.Navigate("https://www.foodwebsite.com/search?q=" + query);
+            }
+        }
+        private void btnBookmark_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.Url != null)
+            {
+                bookmarks.Add(webBrowser1.Url.ToString());
+                listBoxBookmarks.DataSource = new BindingSource(bookmarks, null);
+                SaveBookmarks();
+            }
+            else
+            {
+                MessageBox.Show("No pageis currently loaded to bookmark.");
+            }
+        }
+
+        private void tnOpenBookmark_Click(object sender, EventArgs e)
+        {
+            if (listBoxBookmarks.SelectedItem != null)
+            {
+                webBrowser1.Navigate(listBoxBookmarks.SelectedItem.ToString());
+            }
+        }
+
+        private void btnDeleteBookmark_Click(object sender, EventArgs e)
+        {
+            if (listBoxBookmarks.SelectedItem != null)
+            {
+                bookmarks.Remove(listBoxBookmarks.SelectedItem.ToString());
+                listBoxBookmarks.DataSource = new BindingSource(bookmarks, null);
+                SaveBookmarks();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://google.com");
+        }
+    }
+}
