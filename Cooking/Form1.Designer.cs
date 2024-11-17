16ba941f2fcc041fbd95df01fa4d188137d1aba2
IgorK97 <kt.wert.74@gmail.com>
2024-11-17 20:17:20 +0300

    Дальнейшая разработка, проектирование экранов системы

--- Cooking/Form1.Designer.cs
+++ Cooking/Form1.Designer.cs
@@ -30,19 +30,26 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.txtNote = new System.Windows.Forms.TextBox();
             this.ListBoxNotes = new System.Windows.Forms.ListBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.listBoxBookmarks = new System.Windows.Forms.ListBox();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.txtSearch = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.buttonRes = new System.Windows.Forms.Button();
+            this.buttonSpr = new System.Windows.Forms.Button();
+            this.buttonAboutProg = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -52,14 +59,19 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Enabled = false;
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(944, 426);
+            this.tabControl1.Size = new System.Drawing.Size(1117, 569);
             this.tabControl1.TabIndex = 0;
+            this.tabControl1.Visible = false;
             // 
             // tabPage1
             // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button7);
             this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.richTextBox1);
@@ -68,10 +80,38 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.Size = new System.Drawing.Size(1109, 543);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Заметки";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(7, 7);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(86, 38);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "К главному окну";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(568, 350);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(97, 42);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Добавить";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.btnAddNote_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
@@ -84,29 +124,32 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(204, 34);
+            this.richTextBox1.Location = new System.Drawing.Point(204, 59);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(557, 310);
+            this.richTextBox1.Size = new System.Drawing.Size(557, 285);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
             // txtNote
             // 
-            this.txtNote.Location = new System.Drawing.Point(204, 7);
+            this.txtNote.BackColor = System.Drawing.Color.White;
+            this.txtNote.Location = new System.Drawing.Point(204, 25);
             this.txtNote.Name = "txtNote";
             this.txtNote.Size = new System.Drawing.Size(557, 20);
             this.txtNote.TabIndex = 1;
             // 
             // ListBoxNotes
             // 
+            this.ListBoxNotes.BackColor = System.Drawing.Color.White;
             this.ListBoxNotes.FormattingEnabled = true;
-            this.ListBoxNotes.Location = new System.Drawing.Point(7, 7);
+            this.ListBoxNotes.Location = new System.Drawing.Point(7, 59);
             this.ListBoxNotes.Name = "ListBoxNotes";
-            this.ListBoxNotes.Size = new System.Drawing.Size(190, 381);
+            this.ListBoxNotes.Size = new System.Drawing.Size(190, 329);
             this.ListBoxNotes.TabIndex = 0;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button9);
             this.tabPage2.Controls.Add(this.button6);
             this.tabPage2.Controls.Add(this.button5);
             this.tabPage2.Controls.Add(this.button4);
@@ -117,42 +160,38 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(936, 400);
+            this.tabPage2.Size = new System.Drawing.Size(1109, 543);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Поиск рецептов";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // listBoxBookmarks
+            // button6
             // 
-            this.listBoxBookmarks.FormattingEnabled = true;
-            this.listBoxBookmarks.Location = new System.Drawing.Point(7, 33);
-            this.listBoxBookmarks.Name = "listBoxBookmarks";
-            this.listBoxBookmarks.Size = new System.Drawing.Size(284, 316);
-            this.listBoxBookmarks.TabIndex = 0;
+            this.button6.Location = new System.Drawing.Point(216, 33);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 36);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Добавить закладку";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // webBrowser1
+            // button5
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(297, 7);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(633, 386);
-            this.webBrowser1.TabIndex = 1;
+            this.button5.Location = new System.Drawing.Point(121, 34);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 36);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Удалить закладку";
+            this.button5.UseVisualStyleBackColor = true;
             // 
-            // txtSearch
+            // button4
             // 
-            this.txtSearch.Location = new System.Drawing.Point(7, 7);
-            this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(202, 20);
-            this.txtSearch.TabIndex = 0;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(204, 350);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(82, 43);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Сохранить";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button4.Location = new System.Drawing.Point(7, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 37);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Переименовать закладку";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -162,44 +201,101 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Поиск";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.btnSearchRecipes_Click);
             // 
-            // button4
+            // listBoxBookmarks
             // 
-            this.button4.Location = new System.Drawing.Point(7, 355);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(108, 37);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Переименовать закладку";
-            this.button4.UseVisualStyleBackColor = true;
+            this.listBoxBookmarks.FormattingEnabled = true;
+            this.listBoxBookmarks.Location = new System.Drawing.Point(7, 72);
+            this.listBoxBookmarks.Name = "listBoxBookmarks";
+            this.listBoxBookmarks.Size = new System.Drawing.Size(284, 511);
+            this.listBoxBookmarks.TabIndex = 0;
             // 
-            // button5
+            // webBrowser1
             // 
-            this.button5.Location = new System.Drawing.Point(121, 356);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(84, 36);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Удалить закладку";
-            this.button5.UseVisualStyleBackColor = true;
+            this.webBrowser1.Location = new System.Drawing.Point(297, 33);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(804, 550);
+            this.webBrowser1.TabIndex = 1;
             // 
-            // button6
+            // txtSearch
             // 
-            this.button6.Location = new System.Drawing.Point(211, 358);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 36);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Добавить закладку";
-            this.button6.UseVisualStyleBackColor = true;
+            this.txtSearch.Location = new System.Drawing.Point(7, 7);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(202, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(1025, 7);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "Стили";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // buttonRes
+            // 
+            this.buttonRes.BackColor = System.Drawing.Color.Linen;
+            this.buttonRes.Location = new System.Drawing.Point(49, 593);
+            this.buttonRes.Name = "buttonRes";
+            this.buttonRes.Size = new System.Drawing.Size(75, 39);
+            this.buttonRes.TabIndex = 1;
+            this.buttonRes.Text = "Рецепты";
+            this.buttonRes.UseVisualStyleBackColor = false;
+            this.buttonRes.Click += new System.EventHandler(this.buttonRes_Click);
+            // 
+            // buttonSpr
+            // 
+            this.buttonSpr.BackColor = System.Drawing.Color.Linen;
+            this.buttonSpr.Location = new System.Drawing.Point(146, 593);
+            this.buttonSpr.Name = "buttonSpr";
+            this.buttonSpr.Size = new System.Drawing.Size(75, 39);
+            this.buttonSpr.TabIndex = 2;
+            this.buttonSpr.Text = "Справка";
+            this.buttonSpr.UseVisualStyleBackColor = false;
+            this.buttonSpr.Click += new System.EventHandler(this.buttonSpr_Click);
+            // 
+            // buttonAboutProg
+            // 
+            this.buttonAboutProg.BackColor = System.Drawing.Color.Linen;
+            this.buttonAboutProg.Location = new System.Drawing.Point(245, 593);
+            this.buttonAboutProg.Name = "buttonAboutProg";
+            this.buttonAboutProg.Size = new System.Drawing.Size(90, 39);
+            this.buttonAboutProg.TabIndex = 3;
+            this.buttonAboutProg.Text = "О программе";
+            this.buttonAboutProg.UseVisualStyleBackColor = false;
+            this.buttonAboutProg.Click += new System.EventHandler(this.buttonAboutProg_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Linen;
+            this.buttonExit.Location = new System.Drawing.Point(356, 593);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 39);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            this.buttonExit.Resize += new System.EventHandler(this.buttonExit_Resize);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(959, 450);
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(1129, 644);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonAboutProg);
+            this.Controls.Add(this.buttonSpr);
+            this.Controls.Add(this.buttonRes);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Кулинария";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
@@ -226,6 +322,13 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button buttonRes;
+        private System.Windows.Forms.Button buttonSpr;
+        private System.Windows.Forms.Button buttonAboutProg;
+        private System.Windows.Forms.Button buttonExit;
     }
 }
 
--- Cooking/Form1.cs
+++ Cooking/Form1.cs
@@ -12,15 +12,30 @@ using Microsoft.Win32;
 
 namespace Cooking
 {
+    enum State
+    {
+        StartMenu=0,
+        Note=2,
+        AboutProgram=3,
+        MainWindow,
+        Recipies,
+        Styles,
+        AddingStar
+    }
     public partial class Form1 : Form
     {
         List<string> recipeNotes = new List<string>();
         List<string> bookmarks = new List<string>();
+        State state;
+        Form2 formAddStar;
+        Form4 formSpr;
+        Form5 formAboutProg;
+        Form6 formStyles;
         public Form1()
         {
             InitializeComponent();
             int BrowserVer, RegVal;
-
+            state = State.StartMenu;
             // get the installed IE version
             //using (WebBrowser Wb = new WebBrowser())
                 BrowserVer = webBrowser1.Version.Major;
@@ -42,6 +57,20 @@ namespace Cooking
                 if (Key.GetValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe") == null)
                     Key.SetValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", RegVal, RegistryValueKind.DWord);
             webBrowser1.ScriptErrorsSuppressed = true;
+            buttonRes.Height = this.Size.Height / 10;
+            buttonSpr.Height = this.Size.Height / 10;
+            buttonAboutProg.Height = this.Size.Height / 10;
+            buttonExit.Height = this.Size.Height / 10;
+            buttonRes.Width = this.Size.Width / 5;
+            buttonSpr.Width = this.Size.Width / 5;
+            buttonAboutProg.Width = this.Size.Width / 5;
+            buttonExit.Width = this.Size.Width / 5;
+            buttonRes.Location = new Point(this.Size.Width / 2 - buttonRes.Width / 2, this.Size.Height / 10);
+            buttonSpr.Location = new Point(this.Size.Width / 2 - buttonRes.Width / 2, this.Size.Height / 10*3);
+            buttonAboutProg.Location = new Point(this.Size.Width / 2 - buttonRes.Width / 2, this.Size.Height / 10*5);
+            buttonExit.Location = new Point(this.Size.Width / 2 - buttonRes.Width / 2, this.Size.Height / 10*7);
+            tabControl1.Height = this.Height - 60;
+            tabControl1.Width = this.Width;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -101,7 +130,7 @@ namespace Cooking
             string query = txtSearch.Text;
             if (!string.IsNullOrWhiteSpace(query))
             {
-                webBrowser1.Navigate("https://www.foodwebsite.com/search?q=" + query);
+                webBrowser1.Navigate("https://www.povarenok.ru/recipes/poisk/" + query);
             }
         }
         private void btnBookmark_Click(object sender, EventArgs e)
@@ -139,6 +168,95 @@ namespace Cooking
         private void button3_Click(object sender, EventArgs e)
         {
             webBrowser1.Navigate("http://google.com");
+        }
+
+        private void buttonExit_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (state == State.StartMenu)
+            {
+                buttonRes.Height = this.Size.Height / 10;
+                buttonSpr.Height = this.Size.Height / 10;
+                buttonAboutProg.Height = this.Size.Height / 10;
+                buttonExit.Height = this.Size.Height / 10;
+                buttonRes.Width = this.Size.Width / 5;
+                buttonSpr.Width = this.Size.Width / 5;
+                buttonAboutProg.Width = this.Size.Width / 5;
+                buttonExit.Width = this.Size.Width / 5;
+                buttonRes.Location = new Point(this.Size.Width / 2 - buttonRes.Width / 2, this.Size.Height / 10);
+                buttonSpr.Location = new Point(this.Size.Width / 2 - buttonRes.Width / 2, this.Size.Height / 10 * 3);
+                buttonAboutProg.Location = new Point(this.Size.Width / 2 - buttonRes.Width / 2, this.Size.Height / 10 * 5);
+                buttonExit.Location = new Point(this.Size.Width / 2 - buttonRes.Width / 2, this.Size.Height / 10 * 7);
+            }
+            else
+            {
+                tabControl1.Height = this.Height-60;
+                tabControl1.Width = this.Width;
+            }
+        }
+
+        private void buttonRes_Click(object sender, EventArgs e)
+        {
+            state = State.MainWindow;
+            buttonRes.Visible = false;
+            buttonAboutProg.Visible = false;
+            buttonSpr.Visible = false;
+            buttonExit.Visible = false;
+            buttonRes.Enabled = false;
+            buttonAboutProg.Enabled = false;
+            buttonSpr.Enabled = false;
+            buttonExit.Enabled = false;
+            tabControl1.Enabled = true;
+            tabControl1.Visible = true;
+        }
+
+        private void buttonSpr_Click(object sender, EventArgs e)
+        {
+            formSpr = new Form4();
+            formSpr.ShowDialog();
+        }
+
+        private void buttonAboutProg_Click(object sender, EventArgs e)
+        {
+            formAboutProg = new Form5();
+            formAboutProg.ShowDialog();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            state = State.StartMenu;
+            state = State.MainWindow;
+            buttonRes.Visible = true;
+            buttonAboutProg.Visible = true;
+            buttonSpr.Visible = true;
+            buttonExit.Visible = true;
+            buttonRes.Enabled = true;
+            buttonAboutProg.Enabled = true;
+            buttonSpr.Enabled = true;
+            buttonExit.Enabled = true;
+            tabControl1.Enabled = false;
+            tabControl1.Visible = false;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            formStyles = new Form6();
+            formStyles.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            formAddStar = new Form2();
+            formAddStar.ShowDialog();
         }
     }
 }
